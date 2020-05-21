a4619daa24aa98aa93e0d67ce24ef38101d6f0ad
varalakshmi-iOS <varalakshmi1318@gmail.com>
2020-05-21 08:14:14 +0530

    aaccessing files

---
+++ AccessFiles/fsdfViewController.swift
@@ -0,0 +1,30 @@
+//
+//  fsdfViewController.swift
+//  AccessFiles
+//
+//  Created by Vadde Narendra on 5/6/20.
+//  Copyright © 2020 Narendra Vadde. All rights reserved.
+//
+
+import UIKit
+
+class fsdfViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
